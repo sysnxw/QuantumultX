d85d86def450d32e5c30fa630b5b715823114fe1
sysnxw <44738356+sysnxw@users.noreply.github.com>
2020-07-12 14:44:29 +0800

    Update jd_fruit.js

--- QuantumultX/Task/jd_fruit.js
+++ QuantumultX/Task/jd_fruit.js
@@ -100,11 +100,8 @@ const cookie = $hammer.read('CookieJD')
 const name = '京东水果'
 //助力好友分享码(最多4个,否则后面的助力失败),原因:京东农场每人每天只有四次助力机会
 var shareCodes = [ // 这个列表填入你要助力的好友的shareCode
-    '24d52716a1d043b3b3e30808de3c9abe', //G S
     '039b62a2726644e8830734088a12f6ca', //肯打:chicken:
     'c4a9fc8a1e6645bf9527d39d39ae870a', //tommy
-    '61ff5c624949454aa88561f2cd721bf6',
-    '40dbf12bb7ea4b8eb772741afe2125da'
 ]
 // 添加box功能
 // 【用box订阅的好处】
