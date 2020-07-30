251750a544d07437946e89dcffd50254975948a6
sysnxw <44738356+sysnxw@users.noreply.github.com>
2020-07-30 09:35:13 +0800

    Create q_cookie.js

---
+++ q_cookie.js
@@ -0,0 +1,10 @@
+hostname = pt.btschool.club, www.pthome.net, ngabbs.com
+
+#nga
+^https:\/\/ngabbs.com\/nuke.php\?? - script-request-body https://raw.githubusercontent.com/chouchoui/QuanX/master/Scripts/nga/nga.cookie.js
+
+#pthome
+^https:\/\/www\.pthome\.net\/index\.php - script-request-header https://raw.githubusercontent.com/sysnxw/JsBox/master/pthome/q_pthome.cookie.js
+
+#btschool
+^https:\/\/pt\.btschool\.club\/index\.php - script-request-header https://raw.githubusercontent.com/sysnxw/JsBox/master/btschool/q_btschool.cookie.js
